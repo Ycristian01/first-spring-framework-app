3f605b9e466af6192e098915686ce5dbd5afc300
Ycristian01 <ycristian@uninorte.edu.co>
2023-01-27 16:36:26 -0500

    add relationship between publisher and books

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -23,29 +23,37 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher nor = new Publisher("Norma", "Calle Lagartos #15", "Baq", "Atl", 0000012);
+
+        publisherRepository.save(nor);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "1234567");
         eric.getBoooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(nor);
+        nor.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(nor);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "39345054564");
         rod.getBoooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(nor);
+        nor.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(nor);
 
         System.out.println("Started in bootstrap!");
         System.out.println("Number of books: " + bookRepository.count());
-
-        Publisher nor = new Publisher("Norma", "Calle Lagartos #15", "Baq", "Atl", 0000012);
-
-        publisherRepository.save(nor);
-
-        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("the " + nor.getName() + " publisher has " + nor.getBooks().size() + " books");
     }
 }
